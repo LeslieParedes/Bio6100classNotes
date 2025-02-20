5ab75e6bfbb625f7735c374f193a1dfd9cddd6be
LeslieParedes <lparedes@uvm.edu>
2025-02-20 14:25:48 -0500

    notes

--- ForLoops.R
+++ ForLoops.R
@@ -37,3 +37,79 @@ for (i in 1:length(z)){
   cat("i=",i,"z[i]=",z[i],"\n")
 }
 print(i)
+
+###what if we want to work only the odd numbered elements?
+
+z<- 1:20
+for (i in 1:length(z)){
+  if(i%% 2==0) next
+  print(i)
+}
+
+for (i in 1:length(z)){
+  if(i%% 2!=0) next
+  print(i)
+}
+
+## brake function
+# random walk
+#stochastic random walk
+# input: times =numbers of time steps
+# n1= initial population size (=n[1])
+# lambda =finite rate of increase, 1.0 no change, if 1.1-- 10% increase each time step, 0.98 2% decrease each time step
+# noise_sd = sd of normal distribution with mean 0
+# output: vector n with population sizes>0 until extinction then NA
+
+library(ggplot2)
+ran_walk<-function(times=100, n1=50,lambda=1.00, noise_sd=10){
+  n<-rep(NA,times) #create a output vector
+  n[1]<-n1 #initialize with starting population size
+  noise<-rnorm(n=times,mean=0,sd=noise_sd) #create noise vector
+  for (i in 1:(times-1)) {
+   n[i+1]<-lambda*n[i]+noise[i]
+   if (n[i+1]<=0) {
+     n[i+1]<-NA
+     cat("Population extinction at time", i+1,"\n")
+     break}
+  }
+
+  return(n)
+  }
+
+ran_walk()
+qplot(x=1:100,y=ran_walk(),geom = "line")
+#noise off= line
+qplot(x=1:100,y=ran_walk(noise_sd = 0),geom = "line")
+#adding lambda increase
+qplot(x=1:100,y=ran_walk(noise_sd = 0, lambda = 1.1),geom = "line")
+#adding lambda decrease
+qplot(x=1:100,y=ran_walk(noise_sd = 0, lambda = 0.98),geom = "line")
+
+####double loops
+m<-matrix(round(runif(20),digits = 2),nrow = 5)
+#loop over rows
+for (i in 1:nrow(m)){
+  m[i,]<-m[i,]+1
+}
+print(m)
+
+for (j in 1:ncol(m)){
+  m[j,]<-m[j,]+j
+}
+print(m)
+
+###
+#species area curve
+#create a power function relationship for S and A
+# input: A is a vector of island areas
+#        c is the intercept constant
+        # z is slope constant
+# output: S is a vector of species richness values
+
+species_areas_curve<-function(A=1:5000,c=0.5,z=0.26){
+  S<-c*(A^z)
+  return(S)
+}
+head(species_areas_curve())
+
+#### species area plot
